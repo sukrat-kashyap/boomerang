31ae49e4bd81279dd6140790c037b45951096db9
Sukrat Kashyap <sukraat021195@gmail.com>
2020-06-20 03:55:57 +0100

    feat: adding CICD

--- Gruntfile.js
+++ Gruntfile.js
@@ -766,7 +766,7 @@ module.exports = function() {
 		protractor_webdriver: {
 			options: {
 				keepAlive: true,
-				command: "webdriver-manager start " + webDriverVersions
+				command: "webdriver-manager start " + (webDriverVersions || "")
 			},
 			e2e: {
 			}
